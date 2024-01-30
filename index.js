414aa6bfe0f092c8a954f76aecfcf0b55e9a7c5c
Mahesh <you@example.com>
2024-01-30 11:33:51 +0530

    new changes

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const path = require("path");
 const ejsmate = require("ejs-mate");
 const methodOverride = require("method-override");
 const campground = require("./model/campground");
+const catchAsync = require("./util/catchAsync");
 
 //connect to mongodb
 
@@ -15,7 +16,7 @@ mongoose
   })
   .catch((e) => console.log("error occured connecting to mongodb", e));
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 app.engine("ejs", ejsmate);
 app.set("views", path.join(__dirname, "views"));
@@ -31,53 +32,77 @@ app.get("/campground/new", (req, res) => {
   res.render("Campgrounds/new");
 });
 //new campground
-app.post("/campground", async (req, res) => {
-  const { title, location, Image, description, price } = req.body;
-  const newCamp = await new campground({
-    Title: title,
-    Location: location,
-    image: Image,
-    Description: description,
-    Price: price,
-  }).save();
-  res.redirect(`/campground/${newCamp._id}`);
-});
+app.post(
+  "/campground",
+  catchAsync(async (req, res) => {
+    const { title, location, Image, description, price } = req.body;
+    const newCamp = await new campground({
+      Title: title,
+      Location: location,
+      image: Image,
+      Description: description,
+      Price: price,
+    }).save();
+    res.redirect(`/campground/${newCamp._id}`);
+  })
+);
+
 //edit
-app.get("/campground/:id/edit", async (req, res) => {
-  const { id } = req.params;
-  const campg = await campground.findById(id).exec();
-  res.render("Campgrounds/edit", { campg });
-});
+app.get(
+  "/campground/:id/edit",
+  catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const campg = await campground.findById(id).exec();
+    res.render("Campgrounds/edit", { campg });
+  })
+);
 //All Campgrounds
-app.get("/campground", async (req, res) => {
-  const Campgrounds = await campground.find({});
-  res.render("Campgrounds/index", { Campgrounds });
-});
+app.get(
+  "/campground",
+  catchAsync(async (req, res) => {
+    const Campgrounds = await campground.find({});
+    res.render("Campgrounds/index", { Campgrounds });
+  })
+);
 //show
-app.get("/campground/:id", async (req, res) => {
-  const { id } = req.params;
-  const camp = await campground.findById(id).exec();
-  res.render("Campgrounds/show", { camp });
-});
+app.get(
+  "/campground/:id",
+  catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const camp = await campground.findById(id).exec();
+    res.render("Campgrounds/show", { camp });
+  })
+);
+
 //update
-app.put("/campground/:id", async (req, res) => {
-  const { id } = req.params;
-  const { title, location, image, description, price } = req.body;
-  console.log(image);
-  const updatedCamp = await campground.findByIdAndUpdate(id, {
-    Title: title,
-    Location: location,
-    Image: image,
-    Description: description,
-    Price: price,
-  });
-  res.redirect(`/campground/${updatedCamp._id}`);
-});
+app.put(
+  "/campground/:id",
+  catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const { title, location, image, description, price } = req.body;
+    const updatedCamp = await campground.findByIdAndUpdate(id, {
+      Title: title,
+      Location: location,
+      Image: image,
+      Description: description,
+      Price: price,
+    });
+    res.redirect(`/campground/${updatedCamp._id}`);
+  })
+);
 //delete
-app.delete("/campground/:id", async (req, res) => {
-  const { id } = req.params;
-  await campground.findByIdAndDelete(id);
-  res.redirect("/campground");
+app.delete(
+  "/campground/:id",
+  catchAsync(async (req, res) => {
+    const { id } = req.params;
+    await campground.findByIdAndDelete(id);
+    res.redirect("/campground");
+  })
+);
+
+//middleware
+app.use((err, req, res, next) => {
+  res.send("error occured!");
 });
 app.listen(3000, () => {
   console.log("Listening at port 3000");
